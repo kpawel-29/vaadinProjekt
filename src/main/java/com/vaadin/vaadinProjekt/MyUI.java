53e65d889e4e4cb332333fb6fe102604c25b2344
kpawel-29 <pakadlubowski@gmail.com>
2015-03-30 00:23:58 +0200

    add ColorPicker

--- src/main/java/com/vaadin/vaadinProjekt/MyUI.java
+++ src/main/java/com/vaadin/vaadinProjekt/MyUI.java
@@ -18,10 +18,12 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.colorpicker.Color;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.ColorPicker;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
@@ -32,12 +34,13 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
-
+import com.vaadin.ui.components.colorpicker.ColorChangeEvent;
+import com.vaadin.ui.components.colorpicker.ColorChangeListener;
 
 @Theme("mytheme")
 @Widgetset("com.vaadin.vaadinProjekt.MyAppWidgetset")
 @Push
-public class MyUI extends UI implements MessageListener{
+public class MyUI extends UI implements MessageListener {
 
 	private static final long serialVersionUID = 1L;
 	final VerticalLayout chatContent = new VerticalLayout();
@@ -45,15 +48,15 @@ public class MyUI extends UI implements MessageListener{
 	final Panel chatPanel = new Panel("Chat");
 	final Window window = new Window("Podaj nick");
 	final FormLayout loginForm = new FormLayout();
-	
-	
+	final ColorPicker colorPicker = new ColorPicker();
+
 	@Override
 	protected void init(VaadinRequest vaadinRequest) {
 		final VerticalLayout layout = new VerticalLayout();
 		layout.setMargin(true);
 		setContent(layout);
 
-		final User user = new User(0, "nieznajomy");
+		final User user = new User(0, "nieznajomy", "#0085D5");
 		chatPanel.setHeight("400px");
 
 		window.setWidth(150.0f, Unit.PIXELS);
@@ -61,15 +64,16 @@ public class MyUI extends UI implements MessageListener{
 		window.setClosable(true);
 		window.setDraggable(false);
 		window.setResizable(false);
-		
+
 		final TextField loginField = new TextField("", "");
 		loginField.setWidth(90.0f, Unit.PERCENTAGE);
 		final Button btnOK = new Button("OK");
 		loginForm.addComponent(loginField);
+		loginForm.addComponent(colorPicker);
 		loginForm.addComponent(btnOK);
-		
-	    window.setContent(loginForm);
-	    
+
+		window.setContent(loginForm);
+
 		chatContent.setMargin(true);
 
 		chatPanel.setContent(chatContent);
@@ -79,15 +83,16 @@ public class MyUI extends UI implements MessageListener{
 		header.setHeight("50px");
 		Button btnLogin = new Button("Zaloguj");
 		btnLogin.addClickListener(new Button.ClickListener() {
-			
+
 			@Override
 			public void buttonClick(ClickEvent event) {
 				addWindow(window);
-				
+
 			}
 		});
-		
-		final Label lblWelcome = new Label("Witaj <b>"+user.getName()+"</b>",ContentMode.HTML);
+
+		final Label lblWelcome = new Label("Witaj <b>" + user.getName()
+				+ "</b>", ContentMode.HTML);
 
 		header.addComponent(lblWelcome, "left: 10px; top: 5px;");
 		header.addComponent(btnLogin, "right: 10px; top: 2px;");
@@ -96,28 +101,37 @@ public class MyUI extends UI implements MessageListener{
 		footer.addComponent(messageField);
 		Button btnSend = new Button("Wyślij");
 		footer.addComponent(btnSend);
-		
+
 		footer.setWidth(100.0f, Unit.PERCENTAGE);
 		footer.setExpandRatio(messageField, 1.0f);
 
-		
 		messageField.setWidth(100.0f, Unit.PERCENTAGE);
 
+		colorPicker.setSwatchesVisibility(false);
+		colorPicker.setHistoryVisibility(false);
+		colorPicker.setTextfieldVisibility(false);
+		colorPicker.setHSVVisibility(false);
+		// colorPicker.
+
 		btnOK.addClickListener(new Button.ClickListener() {
-			
+
 			@Override
 			public void buttonClick(ClickEvent event) {
-				window.close();
-				user.setName(loginField.getValue().toString());
-				lblWelcome.setValue("Witaj <b>"+user.getName()+"</b>");
+				if (loginField.isEmpty()) {
+					Notification.show("Error", "Nick nie może być pusty.",
+							Type.TRAY_NOTIFICATION);
+				} else {
+					window.close();
+					user.setName(loginField.getValue().toString());
+					lblWelcome.setValue("Witaj <b>" + user.getName() + "</b>");
+				}
 			}
 		});
 
 		layout.addComponent(header);
 		layout.addComponent(chatPanel);
 		layout.addComponent(footer);
-		
-		
+
 		btnSend.addClickListener(new Button.ClickListener() {
 
 			private static final long serialVersionUID = 1L;
@@ -129,24 +143,32 @@ public class MyUI extends UI implements MessageListener{
 							"Nie możesz wysłać pustej wiadomości",
 							Type.TRAY_NOTIFICATION);
 				} else {
-					MessageBroadcaster.broadcast(new MessageData(messageField.getValue(), user.getName(), new Date()));
+					MessageBroadcaster.broadcast(new MessageData(messageField
+							.getValue(), user.getName(), new Date(), user.getColor()));
 
 				}
 			}
 		});
 		MessageBroadcaster.registerListener(this);
-		
-		
+
+		/*
+		 * http://demo.vaadin.com/sampler/#ui/data-input/other/color-picker
+		 */
+		colorPicker.addColorChangeListener(new ColorChangeListener() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void colorChanged(final ColorChangeEvent event) {
+				Notification.show(
+						"Color changed: " + event.getColor().getCSS(),
+						Type.TRAY_NOTIFICATION);
+				user.setColor(event.getColor().getCSS());
+			}
+
+		});
 
 	}
-	/*
-	 * https://vaadin.com/forum/#!/thread/97069/1359208
-	 */
-/*	private void scrollIntoView() {
-        final VerticalLayout layout = (VerticalLayout)chatContent.getContent();
-        if (getApplication() != null && layout.getComponentCount() > 0)
-            getApplication().getMainWindow().scrollIntoView(layout.getComponent(layout.getComponentCount() - 1));
-    }*/
 
 
 	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
@@ -159,18 +181,19 @@ public class MyUI extends UI implements MessageListener{
 	@Override
 	public void receiveMessage(final MessageData messageData) {
 		access(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				chatContent.addComponent(new Label("("+messageData.getTime()+") <b>" + messageData.getAuthor()
-						+ "</b>: " + messageData.getText(),
-						ContentMode.HTML));
+				chatContent.addComponentAsFirst(new Label("(" + messageData.getTime()
+						+ ") <b>" + messageData.getAuthor()
+						+ "</b>: <font color='" + messageData.getColor() + "'> "
+						+ messageData.getText() + "</font>", ContentMode.HTML));
 				messageField.setValue("");
-/*				chatPanel.scroll*/
+				//System.out.println(java.awt.Color.decode(messageData.getColor()));
+				/* chatPanel.scroll */
 			}
 		});
-		
+
 	}
-		
-	
+
 }
